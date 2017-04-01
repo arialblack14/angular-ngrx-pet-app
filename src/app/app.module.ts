3bf911506cc961fc772726600030bfd71c26e6f4
Alex Tsiras <tsirasalexis@yahoo.gr>
2017-04-02 00:06:47 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './core/app-routing.module';
+import { StoreModule } from '@ngrx/store';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './core/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { CompleteComponent } from './pages/complete/complete.component';
+
+import { petTagReducer } from './core/pet-tag.reducer';
+import { TagShapeComponent } from './pages/create/tag-shape/tag-shape.component';
+import { TagTextComponent } from './pages/create/tag-text/tag-text.component';
+import { TagExtrasComponent } from './pages/create/tag-extras/tag-extras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    CompleteComponent,
+    TagShapeComponent,
+    TagTextComponent,
+    TagExtrasComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    StoreModule.provideStore({ petTag: petTagReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/pet-tag.actions.ts
@@ -0,0 +1,7 @@
+export const SELECT_SHAPE = 'SELECT_SHAPE';
+export const SELECT_FONT = 'SELECT_FONT';
+export const ADD_TEXT = 'ADD_TEXT';
+export const TOGGLE_CLIP = 'TOGGLE_CLIP';
+export const TOGLLE_GEMS = 'TOGLLE_GEMS';
+export const COMPLETE = 'COMPLETE';
+export const RESET = 'RESET';
